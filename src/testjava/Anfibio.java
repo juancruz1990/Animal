4b96ff3f988ff72e18a71202e0fa0267f57cc02a
mero02 <mero2sp@gmail.com>
2017-10-12 18:32:49 -0300

    comente anfibio

--- src/testjava/Anfibio.java
+++ src/testjava/Anfibio.java
@@ -1,6 +1,12 @@
 package testjava;
 
 public class Anfibio extends Animal{
+	/***Constructor de la clase anfibio
+	 * @param vida
+	 * @param ataque
+	 * @param sexo
+	 * @param nombre
+	 */
 	 public Anfibio(int vida , int ataque, char sexo,String nombre){
 		super (vida,ataque,sexo,nombre);
 	    }
