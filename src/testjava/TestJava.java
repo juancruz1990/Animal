6627ffeeb231a6068930becc077ad0d75d35064b
Mariano3031 <cmmalaspina@udc.edu.ar>
2017-10-12 17:01:42 -0300

    modularizacion de la clase testjava

--- src/testjava/TestJava.java
+++ src/testjava/TestJava.java
@@ -14,42 +14,25 @@ public class TestJava {
 	 *            the command line arguments
 	 */
 	public static void main(String[] args) {
-		/*
-		 * Animal p1 = new Pez(100,20,'f',"Pez"); Animal p2 = new
-		 * Pez(100,40,'m',"Pez"); Animal m1 = new
-		 * Mamifero(100,40,'f',"Mamifero"); Animal m2 = new Mamifero(100, 40,
-		 * 'm',"Mamifero"); Mamifero m3 = new Mamifero(100, 10, 'm',"Mamifero");
-		 * Animal m4 = new Mamifero(100, 13, 'f',"Mamifero"); Pez p3 = new
-		 * Pez(100,25, 'm',"Pez"); Pez p4 = new Pez(100,46, 'f',"Pez"); Pez p5 =
-		 * new Pez(100,14,'f',"Pez"); Mamifero m5 = new
-		 * Mamifero(100,31,'m',"Mamifero"); Mamifero m6 = new
-		 * Mamifero(100,46,'m',"Mamifero");
-		 * 
-		 * /* Random r =new Random()
-		 * 
-		 * 
-		 * 
-		 * ArrayList<Animal> l = new ArrayList<>(); { l.add(p1); l.add(p2);
-		 * l.add(p3); l.add(p4); l.add(p5); l.add(m1); l.add(m2); l.add(m3);
-		 * l.add(m4); l.add(m5); l.add(m6); }
-		 * 
-		 * Random r = new Random(); for (int i = 0; i < l.size(); i++) { int
-		 * animalSeleccionado = r.nextInt(l.size() - 1); if (animalSeleccionado
-		 * != i) { l.get(i).comer(l.get(animalSeleccionado));
-		 * 
-		 * } else { System.out.println("No me puedo comer a mi mismo"); } } for
-		 * (int i = 0; i < l.size(); i++) {
-		 * System.out.println(l.get(i).getVida() +
-		 * " soy "+l.get(i).getNombre()+"  "+l.get(i).getEstado()); }
-		 */
 		Random r = new Random();
 		ArrayList<Animal> listaAnimales = new ArrayList<Animal>();
+		listaAnimales = generacionAleatoria(listaAnimales, r, 1000);
+		simulacion(listaAnimales, r);
+	}
+	/***
+	 * genera aleatoriamente una cantidad de animales pasada por parametros
+	 * @param listaAnimales arraylist de animales
+	 * @param r objeto de la clase random 
+	 * @param cantidad de elementos que se quieren crear
+	 * @return
+	 */
+	static ArrayList<Animal> generacionAleatoria(ArrayList<Animal> listaAnimales, Random r, int cantidad){
 		int valor;
 		char sexo;
 		int vida;
 		int ataque;
 		Animal p1;
-		for (int i=0 ; i<10000 ; i++){
+		for (int i=0 ; i<cantidad ; i++){
 			valor = r.nextInt(5) + 1;
 			if (r.nextInt(100) % 2 == 0){
 				sexo = 'f';
@@ -80,7 +63,16 @@ public class TestJava {
 				listaAnimales.add(p1);
 			}
 		}
+		return listaAnimales;
+	}
+	/***
+	 * simulacion hace que los animales contenidos en el arraylist se coman entre ellos
+	 * @param listaAnimales
+	 * @param r
+	 */
+	static void simulacion(ArrayList<Animal> listaAnimales, Random r){
 		int j=0, k=0;
+		int valor;
 		while ((listaAnimales.size())>1){
 			do{
 				valor=r.nextInt(listaAnimales.size());
@@ -103,6 +95,5 @@ public class TestJava {
 				j=0;
 			}
 		}
-		System.out.println("termine de ejecutarme");
 	}
 }
